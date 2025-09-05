b9a62467d04e8967760d3de3109b1a6b815bdefa
Toni Hermoso Pulido <toni.hermoso@crg.eu>
2025-09-05 12:51:11 +0200

    Upgrade hooks

--- SMWFileProtect.hooks.php
+++ SMWFileProtect.hooks.php
@@ -2,14 +2,14 @@
 
 class SMWFileProtectHooks
 {
-    public static function SMWProtectuserCan($title, $user, $action, &$result)
+    public static function onGetUserPermissionsErrors($title, $user, $action, &$result)
     {
         $object = new SMWFileProtect();
         $result = $object->executeImageRefer($title, $user);
         return $result;
     }
 
-    public static function SMWProtectNSuserCan($title, $user, $action, &$result)
+    public static function onGetUserPermissionsErrorsNS($title, $user, $action, &$result)
     {
         $object = new SMWNSProtect();
         $result = $object->executeNSRefer($title, $user);
